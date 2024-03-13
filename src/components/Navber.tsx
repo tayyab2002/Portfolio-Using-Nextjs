5cb2a7ab00a6b1c3b3daa987f9b77ad63864f6a5
Tayyab Khattak <tayyabktk2002@gmail.com>
2024-03-13 05:49:39 +0500

    Startup

---
+++ src/components/Navber.tsx
@@ -0,0 +1,15 @@
+'use clinet'
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+const Navber = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >Navber</div>
+  )
+}
+
+export default Navber;
